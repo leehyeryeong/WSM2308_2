1c8f2606496b3c332c2b7995b98e5d56d04afc32
SSep <ssep4u@gmail.com>
2022-10-19 12:01:00 +0900

    급식 API 호출 url 출력

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -77,3 +77,21 @@ const initButton = () => {
     next_btn.onclick = nextMonth;
 }
 initButton();
+
+//급식API, AJAX 급식데이터 가져오자
+const KEY = "d1dc39d63a314f2387d2dbd1c732b392";
+const ATPT_OFCDC_SC_CODE = "B10";   //서울특별시교육청
+const SD_SCHUL_CODE = "7010569";    //미림여자정보과학고등학교
+let MLSV_YMD = "20221019";          //2022년 10월 19일
+let MMEAL_SC_CODE = 3;              //중식
+let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+url += `?KEY=${KEY}`;
+url += `&Type=json`;
+url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+url += `&MLSV_YMD=${MLSV_YMD}`;
+url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
+console.log(url);
+
+
+
