cb39c4155461d3cbd573887695ba4165748b8755
SSep <ssep4u@gmail.com>
2022-10-24 12:07:20 +0900

    js 마우스 이벤트 처리

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -80,19 +80,35 @@ initButton();
 
 //급식API, AJAX 급식데이터 가져오자
 //.date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하자
-//handler에서 year, month, date 정보를 가져와서 url 생성하자
+const handler = (event) => {
+    //handler에서 year, month, date 정보를 가져와서 url 생성하자
+    let date = event.target.innerHTML;
+    const KEY = "d1dc39d63a314f2387d2dbd1c732b392";
+    const ATPT_OFCDC_SC_CODE = "B10";   //서울특별시교육청
+    const SD_SCHUL_CODE = "7010569";    //미림여자정보과학고등학교
+    let MLSV_YMD = `${year}${month.toString().padStart(2, "0")}${date.padStart(2, "0")}`;
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+    console.log(url);
+    getMenuByAPI(url);
+}
 //AJAX로 url 호출하자(Asynchronous JavaScript And XML)
+const getMenuByAPI = (url) => {
+    //XMLHttpRequest 만들자
+    //요청을 보낼 방식, url, 비동기여부 설정하자
+    //요청 전송하자
+    //callback
+}
 //응답오면, #breakfast, #lunch, #dinner에 출력하자
-const KEY = "d1dc39d63a314f2387d2dbd1c732b392";
-const ATPT_OFCDC_SC_CODE = "B10";   //서울특별시교육청
-const SD_SCHUL_CODE = "7010569";    //미림여자정보과학고등학교
-let MLSV_YMD = "20221019";          //2022년 10월 19일
-let MMEAL_SC_CODE = 3;              //중식
-let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-url += `?KEY=${KEY}`;
-url += `&Type=json`;
-url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
-url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
-url += `&MLSV_YMD=${MLSV_YMD}`;
-url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
-console.log(url);
+let dateGridContainerDiv = document.getElementsByClassName("date-grid-container")[0];
+let gridItems = dateGridContainerDiv.getElementsByClassName("grid-item");
+for (let gridItem of gridItems) {
+    // console.log(gridItem);
+    gridItem.onclick = handler;
+}
+
+
